1bc8d3422750980d4e07c5f38e21f359aaa27942
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-06-04 20:17:53 -0500

    updates on links for pictures

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ const sequelize = process.env.JAWSDB_URL
 
     process.env.DB_NAME = 'brewery_db',
     process.env.DB_USER = 'root',
-    process.env.DB_PW = 'password',
+    process.env.DB_PW = 'monterrey1$M',
 
     {
       host: 'localhost',
