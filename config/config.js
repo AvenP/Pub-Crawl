3a34b791dd1684dd08aab3af98e67db2c0bc2739
Christopher <christopherperez@Christophers-Laptop.local>
2023-06-06 07:29:22 -0500

    make it better

--- config/config.js
+++ config/config.js
@@ -14,12 +14,29 @@ const sequelize = process.env.JAWSDB_URL
 
     process.env.DB_NAME = 'brewery_db',
     process.env.DB_USER = 'root',
-    process.env.DB_PW = 'monterrey1$M',
+    process.env.DB_PW = 'WangPerez198679!',
 
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3001
     });
 
 module.exports = sequelize;
+
+// /////////////////////////////////////
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3001
+//     });
+// }
+// module.exports = sequelize;
